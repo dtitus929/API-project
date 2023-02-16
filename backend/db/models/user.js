b4b23d21fbd39222de4788f1bd659e4bdf349cfe
dtitus929 <dave@creativegozone.com>
2023-02-16 15:48:05 -0500

    Add User model authentication Sequelize scopes and methods

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -4,16 +4,49 @@ const bcrypt = require('bcryptjs');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
+
     toSafeObject() {
       const { id, username, email } = this; // context will be the User instance
       return { id, username, email };
-    };
+    }
+
     validatePassword(password) {
       return bcrypt.compareSync(password, this.hashedPassword.toString());
     }
+
     static associate(models) {
       // define association here
     }
+
+    static getCurrentUserById(id) {
+      return User.scope("currentUser").findByPk(id);
+    }
+
+    static async login({ credential, password }) {
+      const { Op } = require('sequelize');
+      const user = await User.scope('loginUser').findOne({
+        where: {
+          [Op.or]: {
+            username: credential,
+            email: credential
+          }
+        }
+      });
+      if (user && user.validatePassword(password)) {
+        return await User.scope('currentUser').findByPk(user.id);
+      }
+    }
+
+    static async signup({ username, email, password }) {
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({
+        username,
+        email,
+        hashedPassword
+      });
+      return await User.scope('currentUser').findByPk(user.id);
+    }
+
   };
 
   User.init(
