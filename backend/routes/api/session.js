eabb68f5c2d694ed95cbfd13d33bc4ef6b53c5be
dtitus929 <dave@creativegozone.com>
2023-02-21 15:25:55 -0500

    Sign-up, Sign-in, Get Current User All Good

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -10,10 +10,10 @@ const validateLogin = [
     check('credential')
         .exists({ checkFalsy: true })
         .notEmpty()
-        .withMessage('Please provide a valid email or username.'),
+        .withMessage('Email or username is required'),
     check('password')
         .exists({ checkFalsy: true })
-        .withMessage('Please provide a password.'),
+        .withMessage('Password is required'),
     handleValidationErrors
 ];
 
@@ -29,7 +29,7 @@ router.post(
         const user = await User.login({ credential, password });
 
         if (!user) {
-            const err = new Error('Login failed');
+            const err = new Error('Invalid credentials');
             err.status = 401;
             err.title = 'Login failed';
             err.errors = { credential: 'The provided credentials were invalid.' };
