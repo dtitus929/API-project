6c78e78423a3c9b62414be1d53bef20168b7708d
dtitus929 <dave@creativegozone.com>
2023-03-14 18:55:30 -0400

    New 'Open Sans" Font

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -29,7 +29,7 @@ export default function Home() {
                     <Link key={id} title={name} className='spot-card-link' to={`/spots/${id}`}>
                         <div className="spot-card" style={{ backgroundImage: '' }}>
                             <img src={previewImage} alt={id} style={{ borderRadius: '14px', marginBottom: '8px' }} />
-                            <div style={{ fontWeight: '400', fontSize: '15px' }}>{city}, {state}<span style={{ float: 'right', fontWeight: '300' }}><i className="fa-solid fa-star" style={{ color: '#993399' }} />&nbsp;{avgRating === 'No reviews yet' ? 'New' : Number(avgRating).toFixed(1)}</span></div>
+                            <div style={{ fontWeight: '500', fontSize: '15px' }}>{city}, {state}<span style={{ float: 'right', fontWeight: '300' }}><i className="fa-solid fa-star" style={{ color: '#993399' }} />&nbsp;{avgRating === 'No reviews yet' ? 'New' : Number(avgRating).toFixed(1)}</span></div>
                             <div style={{ fontWeight: '400', paddingTop: '5px' }}>${Number(price).toFixed(2)}&nbsp;<span style={{ fontWeight: '300' }}>night</span></div>
                         </div>
                     </Link>
