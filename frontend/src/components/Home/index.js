7313aba4a767a6de48e4d9e1c47213116fd95f59
dtitus929 <dave@creativegozone.com>
2023-03-14 20:12:22 -0400

    Made Spot Card Component

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,7 +1,7 @@
 
 import { useEffect } from 'react';
-import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
+import Card from '../Card/card'
 
 import { getSpots } from '../../store/spots';
 
@@ -24,16 +24,7 @@ export default function Home() {
 
         <div className="spot-holder">
             <div className='spot-card-holder'>
-
-                {arrSpots?.map(({ id, name, city, state, avgRating, price, previewImage }) => (
-                    <Link key={id} title={name} className='spot-card-link' to={`/spots/${id}`}>
-                        <div className="spot-card" style={{ backgroundImage: '' }}>
-                            <img src={previewImage} alt={id} style={{ borderRadius: '14px', marginBottom: '8px' }} />
-                            <div style={{ fontWeight: '500', fontSize: '15px' }}>{city}, {state}<span style={{ float: 'right', fontWeight: '300' }}><i className="fa-solid fa-star" style={{ color: '#993399' }} />&nbsp;{avgRating === 'No reviews yet' ? 'New' : Number(avgRating).toFixed(1)}</span></div>
-                            <div style={{ fontWeight: '400', paddingTop: '5px' }}>${Number(price).toFixed(2)}&nbsp;<span style={{ fontWeight: '300' }}>night</span></div>
-                        </div>
-                    </Link>
-                ))}
+                <Card arrSpots={arrSpots} showUpdateDelete={false} />
             </div>
         </div>
     );
