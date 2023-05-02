4bae42340dd0052d33a8254be477694beb4a660f
dtitus929 <dave@creativegozone.com>
2023-05-02 18:57:13 -0400

    Updated Footer Links to Portfolio Github LinkedIn

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -6,7 +6,48 @@ export default function Footer() {
         <div className="footer-holder">
             <div className="footer">
                 <div>© {new Date().getFullYear()} Aircnc, Inc · <span className="fake-footer-link">Terms</span> · <span className="fake-footer-link">Sitemap</span> · <span className="fake-footer-link">Privacy</span></div>
-                <div><i className="fa-solid fa-globe" />&nbsp;English (US)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$ USD&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i className="fa-solid fa-envelope" />&nbsp;&nbsp;&nbsp;<i className="fa-solid fa-location-dot" />&nbsp;&nbsp;&nbsp;<i className="fa-solid fa-share" /></div>
+
+
+                <div style={{ display: 'flex', alignItems: 'center', marginTop: '5px' }}>
+
+                    <div style={{ marginBottom: '2px' }}>
+                        <span>Dave Titus:&nbsp;</span>
+                    </div>
+
+                    <div className="footer-link">
+                        <span>
+                            <a className="footer-button" href="http://creativegozone.com/" target="_blank" rel="noreferrer">
+                                <button className="copyright-button">
+                                    <i className="fas fa-eye" style={{ fontSize: '14px' }}></i>
+                                </button>
+                            </a>
+                        </span>
+                    </div>
+
+                    <div className="footer-link">
+                        <span>
+                            <a className="footer-button" href="https://github.com/dtitus929/AirCnC" target="_blank" rel="noreferrer">
+                                <button className="copyright-button">
+                                    <i className="fa fa-github" style={{ fontSize: '14px' }}></i>
+                                </button>
+                            </a>
+                        </span>
+                    </div>
+
+                    <div className="footer-link">
+                        <span>
+                            <a className="footer-button" href="https://www.linkedin.com/in/djtitus/" target="_blank" rel="noreferrer">
+                                <button className="copyright-button">
+                                    <i className="fa fa-linkedin-square" style={{ fontSize: '14px' }}></i>
+                                </button>
+                            </a>
+                        </span>
+                    </div>
+
+                </div >
+
+                {/* <div><i className="fa-solid fa-globe" />&nbsp;English (US)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$ USD</div> */}
+
             </div>
         </div>
 
